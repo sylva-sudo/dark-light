4958c76a548f33eee906be1cb3c25f1d3e398181
sylva-sudo <sylvaaditya80@gmail.com>
2023-08-22 09:16:24 +0700

    dark&light animation

---
+++ WEH/script.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('animContainer'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://lottie.host/b0046608-b2dc-4f06-b595-b5125ee9a40c/xBKGn1Ohoo.json'
+})
